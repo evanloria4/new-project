a71b6110dc7334f09d0de723fa4f0db58ba5a2b9
Evan Loria <evan.loria4@gmail.com>
2026-05-27 13:54:40 -0500

    Update repo using Claude for more ready production

--- src/server/app.ts
+++ src/server/app.ts
@@ -8,4 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static(DIST_DIR));
 
+app.get('/{*splat}', (_req, res) => {
+  res.sendFile(path.join(DIST_DIR, 'index.html'));
+});
+
 export default app;
